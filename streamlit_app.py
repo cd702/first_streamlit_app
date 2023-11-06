fb442b585145cc8861bd56d74fdd23ddb6996118
Cora Dutsch Torres <62064917+cd702@users.noreply.github.com>
2023-11-06 19:00:15 +0000

    Update streamlit_app.py
    something is wrong with df displays

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@
 import streamlit
 import pandas as pd
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
   
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('Breakfast')
@@ -13,4 +12,5 @@ streamlit.text('🥗 Kale Smoothie')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)

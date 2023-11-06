eb52becf41dd0c90a87056cde1ff91f0932211fb
Cora Dutsch Torres <62064917+cd702@users.noreply.github.com>
2023-11-06 18:50:35 +0000

    Update streamlit_app.py
    add blueberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('Breakfast')
+streamlit.text('Blueberry Oats')
 streamlit.text('Hard Boiled Eggs')
 streamlit.text('Kale Smoothie')
-streamlit.text('Blueberry Oats')
